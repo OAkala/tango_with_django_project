4aa1d9dd9579fee50cf53c7574f69eb50fa20ed8
Olalekan Akala <2514445A@student.gla.ac.uk>
2020-02-14 10:41:56 +0000

    Added user login restrictions

--- rango/views.py
+++ rango/views.py
@@ -34,6 +34,7 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
     return render(request, 'rango/category.html', context = context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -48,6 +49,7 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug = category_name_slug)
@@ -123,3 +125,11 @@ def user_login(request):
     else:
         return render(request, 'rango/login.html')
 
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
